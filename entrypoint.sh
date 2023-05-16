adf689d6e2e0994699ad81f964d3091da2273357
alexb4a <120390853+alexb4a@users.noreply.github.com>
2023-05-16 14:19:25 -0300

    Create entrypoint.sh

---
+++ entrypoint.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+set -e
+
+# Mount S3 bucket
+s3fs my-bucket-name /mnt/s3 -o passwd_file=/etc/passwd-s3fs -o umask=022
+
+# Start Jenkins
+java -Djenkins.home=/mnt/s3 -jar /usr/share/jenkins/jenkins.war &
+
+# Keep the container running
+tail -f /dev/null
