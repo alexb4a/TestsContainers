e50790f8f9c4e610af649de3fb6feda928705548
alexb4a <120390853+alexb4a@users.noreply.github.com>
2023-05-16 14:31:51 -0300

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,7 +2,7 @@
 set -e
 
 # Mount S3 bucket
-s3fs my-bucket-name /mnt/s3 -o passwd_file=/etc/passwd-s3fs -o umask=022
+s3fs com.back4app.jenkinsalex /mnt/s3 -o passwd_file=/etc/passwd-s3fs -o umask=022
 
 # Start Jenkins
 java -Djenkins.home=/mnt/s3 -jar /usr/share/jenkins/jenkins.war &
